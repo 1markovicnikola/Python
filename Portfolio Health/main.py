77ec4f9133a67db88a758085b7bdb88d4f7ee552
1markovicnikola <104412007+1markovicnikola@users.noreply.github.com>
2022-05-04 20:53:49 +0530

    Add files via upload

---
+++ Portfolio Health/main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import nsepy as nse
+
+
+
+def LTP(symbol_):
+    market = nse.get_quote(symbol = symbol_)
+    return market['data'][0]['lastPrice']
+
+
+
+df = pd.read_csv('../investment.csv')
+lst = [LTP(ticker) for ticker in list(df['TICKER'])]
+ltp_without_commas = [float(ltp.replace(',', '')) for ltp in lst]
+df['LTP'] = ltp_without_commas
+#df['INVESTED'] = [df['QUANTITY'][i]*df['BUYPRICE'][i] for i in range(len(df['TICKER']))]
+
+
+df['INVESTED'] = df['QUANTITY']*df['BUYPRICE']
+
+df['PERCENTAGE'] = (df['LTP']/df['BUYPRICE'] - 1)*100
+
+df['PROFITLOSS'] = (df['LTP'] - df['BUYPRICE'])*(df['QUANTITY'])
+
+print(df)
+print('Total Investment:', df['INVESTED'].sum())
+print('Profit & Loss:', round(df['PROFITLOSS'].sum(), 2))
+print('Net Percentage:', round(df['PROFITLOSS'].sum()*100/df['INVESTED'].sum(), 2))
